cf4111c8d63276b604816225aa7e98d98cf5b00c
juliauzbemb <90088899+juliauzbemb@users.noreply.github.com>
2023-04-07 18:51:36 +0300

    function and tests updated

--- src/js/__tests__/cleanfunc.test.js
+++ src/js/__tests__/cleanfunc.test.js
@@ -3,15 +3,11 @@ import getHeroHealth from '../cleanfunc';
 test.each([
   [{ name: 'Mar', health: 30 }, 'wounded'],
   [{ name: 'Mar', health: 14 }, 'critical'],
+  [{ name: 'Mar', health: 15 }, 'wounded'],
+  [{ name: 'Mar', health: 50 }, 'wounded'],
   [{ name: 'Mar', health: 60 }, 'healthy'],
-  [{ name: 'Mar', health: 0 }, 'dead'],
-  [{ name: 'Mar', health: 'some string' }, null],
-  [{ name: undefined, health: 40 }, null],
-  [{ name: 'Mar', health: undefined }, null],
-  [{ name: 'Mar', health: -10 }, 'dead'],
+  [{ name: 'Mar', health: 0 }, 'critical'],
   [{ name: 'Mar', health: 1000000000000 }, 'healthy'],
-  [{ name: '', health: 10 }, null],
-  [{ name: 'Mar', health: '50' }, null],
 ])(
   ('should get health status for object %o'),
   (heroObject, expected) => {
--- src/js/cleanfunc.js
+++ src/js/cleanfunc.js
@@ -1,19 +1,9 @@
 export default function getHeroHealth(object) {
-  let result = null;
-
-  if (object.name === undefined || object.name === '' || object.health === undefined || typeof object.health === 'string') {
-    return null;
-  }
-
   if (object.health > 50) {
-    result = 'healthy';
-  } else if (object.health >= 15 && object.health <= 50) {
-    result = 'wounded';
-  } else if (object.health < 15 && object.health > 0) {
-    result = 'critical';
-  } else {
-    result = 'dead';
+    return 'healthy';
   }
-
-  return result;
+  if (object.health >= 15 && object.health <= 50) {
+    return 'wounded';
+  }
+  return 'critical';
 }
